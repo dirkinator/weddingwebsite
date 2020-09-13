1ee55e11a326cd2e4baec1eda4ad5b16ad032b0d
Dirk Blaauw <bier@brouwbroeders.nl>
2020-09-13 16:09:24 +1000

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  default_url_options :host => "www.dirkenstefaniegaantrouwen.herokuapp.com"
 
   get 'welcome/index'
 
