d5b58bb234262de92ade541ef75b3ac98cde2355
Dirk Blaauw <bier@brouwbroeders.nl>
2020-09-13 12:07:33 +1000

    wedding website

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@
 
 class WelcomeController < ApplicationController
   def index
-    @google_maps_url = 'https://goo.gl/maps/nBAxNAsmPSS2'
-    @cool_earth_url = 'https://www.coolearth.org/'
+    @google_maps_url = 'https://www.google.com/maps/place/Chateau+de+Puissentut/@43.81041,0.8218471,17z/data=!4m8!3m7!1s0x0:0x1707c0843e6e42ba!5m2!4m1!1i2!8m2!3d43.8104062!4d0.824041'
   end
 end
