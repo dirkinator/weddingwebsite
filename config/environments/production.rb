2189620a693130ba91f6e3cd19386a2000c1365d
Dirk Blaauw <bier@brouwbroeders.nl>
2020-09-13 18:44:37 +1000

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,12 +74,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: (ENV['SMTP_PORT'] || 587).to_i,
-    domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USER_NAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: ENV['SMTP_AUTHENTICATION'] || 'plain',
     enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] || 'true',
-    openssl_verify_mode: ENV['SMTP_OPENSSL'],
     tls: true
   }
 
