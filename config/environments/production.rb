507ca8b6d91c76172ce8bb283b9c31df34137d92
Dirk Blaauw <bier@brouwbroeders.nl>
2020-09-13 18:39:32 +1000

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,8 @@ Rails.application.configure do
     password: ENV['SMTP_PASSWORD'],
     authentication: ENV['SMTP_AUTHENTICATION'] || 'plain',
     enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] || 'true',
-    openssl_verify_mode: ENV['SMTP_OPENSSL']
+    openssl_verify_mode: ENV['SMTP_OPENSSL'],
+    tls: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
